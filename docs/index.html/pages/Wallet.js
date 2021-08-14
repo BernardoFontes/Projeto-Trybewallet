14720c19debf60cc3adf60cf941056c221997f10
BernardoFontesNKWEB <bernardo.miranda@nkweb.com.br>
2021-08-13 23:07:04 -0300

    a

---
+++ docs/index.html/pages/Wallet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Formulario from './Formulario';
+
+class Wallet extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Formulario />
+      </div>
+    );
+  }
+}
+
+export default Wallet;
---
+++ docs/index.html/reducers/wallet.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+    currencies: [],
+    expenses: [],
+  };
+  
+  function wallet(state = INITIAL_STATE, action) {
+    switch (action.type) {
+    case 'addDespesa':
+      return ({
+        ...state,
+        expenses: [...state.expenses, action.value],
+      });
+    default:
+      return state;
+    }
+  }
+  
+  export default wallet;
